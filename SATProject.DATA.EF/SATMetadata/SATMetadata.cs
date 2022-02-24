45b7ef6b37869105a29ba60942a769766c605f7d
scash58 <scashion58@gmail.com>
2022-02-24 09:08:12 -0600

    Added metadata to student and enrollment data tables.

--- SATProject.DATA.EF/SATMetadata/SATMetadata.cs
+++ SATProject.DATA.EF/SATMetadata/SATMetadata.cs
@@ -48,8 +48,18 @@ namespace SATProject.DATA.EF//.SATMetadata
 
     public class EnrollmentMetaData
     {
+        //public int EnrollmentId { get; set; }
+        //public int StudentId { get; set; }
+        //public int ScheduledClassId { get; set; }
 
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Enrollment Date")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true, NullDisplayText = "[-N/A-]")]
+        public System.DateTime EnrollmentDate { get; set; }
     }
+
+    [MetadataType(typeof(EnrollmentMetaData))]
+    public partial class Enrollment { }
 
     #endregion
 
@@ -75,7 +85,62 @@ namespace SATProject.DATA.EF//.SATMetadata
 
     public class StudentMetaData
     {
+        //public int StudentId { get; set; }
 
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(20, ErrorMessage = "* Value must be 20 characters or less.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(20, ErrorMessage = "* Value must be 20 characters or less.")]
+        public string LastName { get; set; }
+
+        [DisplayFormat(NullDisplayText = "[-N/A-]")]
+        [StringLength(15, ErrorMessage = "* Value must be 15 characters or less.")]
+        public string Major { get; set; }
+
+        [DisplayFormat(NullDisplayText = "[-N/A-]")]
+        [StringLength(50, ErrorMessage = "* Value must be 50 characters or less.")]
+        public string Address { get; set; }
+
+        [DisplayFormat(NullDisplayText = "[-N/A-]")]
+        [StringLength(25, ErrorMessage = "* Value must be 25 characters or less.")]
+        public string City { get; set; }
+
+        [DisplayFormat(NullDisplayText = "[-N/A-]")]
+        [StringLength(2, ErrorMessage = "* Value must be 2 characters or less.")]
+        public string State { get; set; }
+
+        [DisplayFormat(NullDisplayText = "[-N/A-]")]
+        [StringLength(10, ErrorMessage = "* Value must be 10 characters or less.")]
+        public string ZipCode { get; set; }
+
+        [DisplayFormat(NullDisplayText = "[-N/A-]")]
+        [StringLength(13, ErrorMessage = "* Value must be 13 characters or less.")]
+        public string Phone { get; set; }
+
+        [Required]
+        [StringLength(60, ErrorMessage = "* Value must be 60 characters or less.")]
+        public string Email { get; set; }
+
+        [DisplayFormat(NullDisplayText = "[-N/A-]")]
+        [StringLength(100, ErrorMessage = "* Value must be 100 characters or less.")]
+        public string PhotoUrl { get; set; }
+
+        [Required]
+        public int SSID { get; set; }
+    }
+
+    [MetadataType(typeof(StudentMetaData))]
+    public partial class Student
+    {
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
     }
 
     #endregion
